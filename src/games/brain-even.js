6ab0827fcab2e19071da2a51f5b37438bebef43b
Denian-Muir <121126045+Denian-Muir@users.noreply.github.com>
2023-01-19 10:22:05 +0300

    Create brain-even.js

---
+++ src/games/brain-even.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import Game from '../index.js';
+import randomNumber from '../randomNumber.js';
+
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const number = (num) => num % 2 === 0;
+const questionandAnswer = () => {
+  const question = randomNumber();
+  const answer = number(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+const GameEven = () => {
+  Game(rules, questionandAnswer);
+};
+export default GameEven;
